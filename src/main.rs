4daa59f4c4fb4db30c3a279dddc6b20c82fc0eb9
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-01-16 16:36:07 +0100

    Add some sample programm and try to read it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    #[arg(short, long)]
+    file: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let Ok(file_content) = std::fs::read_to_string(&args.file) else {
+        println!("Could not read file: '{}'", args.file.to_string_lossy());
+        std::process::exit(-1);
+    };
+
+    println!("{}", file_content);
 }
