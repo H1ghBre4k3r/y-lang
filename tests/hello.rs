cc91fd35cbe645f7eb36103b11552e6b65eeb680
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-02-06 17:14:25 +0100

    Install NASM in github workflow

--- tests/hello.rs
+++ tests/hello.rs
@@ -20,11 +20,11 @@ fn compile_and_run_hello() -> Result<(), Box<dyn Error>> {
         .args(["-f", FILE_NAME, "-c", "-o", "./output/hello"])
         .output()?;
 
-    assert_eq!(output.stderr, vec![]);
+    assert_eq!(std::str::from_utf8(&output.stderr)?, "");
 
     let output = Command::new("./output/hello").output()?;
 
-    assert_eq!(output.stdout, "Hello, World!".to_owned().into_bytes());
-    assert_eq!(output.stderr, vec![]);
+    assert_eq!(std::str::from_utf8(&output.stdout)?, "Hello, World!");
+    assert_eq!(std::str::from_utf8(&output.stderr)?, "");
     Ok(())
 }
