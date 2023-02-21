aab72a34c1e62c69cdbe2041c1ad27d3ad6b27a1
fwcd <fwcdmail@gmail.com>
2023-02-21 20:37:22 +0100

    Migrate expressions test to new structure

--- tests/expressions.rs
+++ tests/expressions.rs
@@ -1,32 +1,19 @@
-use std::{error::Error, process::Command};
+use std::{error::Error, path::Path};
 
-const FILE_NAME: &str = "./examples/expressions.why";
-const EXPECTED_OUTPUT: &str = "22 39 -201";
+use test_utils::{check_compilation, check_interpretation, Expected};
+
+const SRC_PATH: &str = "./examples/expressions.why";
+const EXPECTED: Expected = Expected {
+    stdout: "22 39 -201",
+    stderr: "",
+};
 
 #[test]
 fn interpret_expressions() -> Result<(), Box<dyn Error>> {
-    let output = Command::new("./target/debug/why")
-        .args(["-f", FILE_NAME, "-r"])
-        .output()?;
-
-    assert_eq!(std::str::from_utf8(&output.stdout)?, EXPECTED_OUTPUT);
-    assert_eq!(std::str::from_utf8(&output.stderr)?, "");
-
-    Ok(())
+    check_interpretation(Path::new(SRC_PATH), EXPECTED)
 }
 
 #[test]
 fn compile_and_run_expressions() -> Result<(), Box<dyn Error>> {
-    let output = Command::new("./target/debug/why")
-        .args(["-f", FILE_NAME, "-o", "./output/expressions"])
-        .output()?;
-
-    println!("{}", std::str::from_utf8(&output.stdout)?);
-    assert_eq!(std::str::from_utf8(&output.stderr)?, "");
-
-    let output = Command::new("./output/expressions").output()?;
-
-    assert_eq!(std::str::from_utf8(&output.stdout)?, EXPECTED_OUTPUT);
-    assert_eq!(std::str::from_utf8(&output.stderr)?, "");
-    Ok(())
+    check_compilation(Path::new(SRC_PATH), EXPECTED)
 }
