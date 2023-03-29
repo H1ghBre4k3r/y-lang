f96e0699b76465f324ae41223be8b2f1ac6047d9
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-03-29 19:45:26 +0200

    Add some kind of first LSP integration

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -31,7 +31,7 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
-    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    documentSelector: [{ scheme: "file", language: "why" }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
--- src/bin/lsp.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    dbg!("Hello, world!");
-    loop {}
-}
