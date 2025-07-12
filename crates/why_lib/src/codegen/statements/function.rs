b1f096681e9ec19548597d77525c173bd1fdc307
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-07-12 17:15:17 +0200

    feat(codegen): introduce scoped storage for variables and functions
    Refactor codegen context to use a unified scope stack that stores both
    variables and functions in ScopeFrame structs. This replaces the previous
    separate variable stack with a more structured approach, enabling proper
    scoping and lookup of functions alongside variables.
    
    Update function codegen to store function values in the current scope and
    enter a new scope for parameters and local variables. This change improves
    symbol resolution and prepares for better support of nested scopes and
    function references during code generation.

--- crates/why_lib/src/codegen/statements/function.rs
+++ crates/why_lib/src/codegen/statements/function.rs
@@ -2,7 +2,7 @@ use inkwell::types::{BasicMetadataTypeEnum, FunctionType};
 
 use crate::{
     codegen::{CodeGen, CodegenContext},
-    parser::ast::Function,
+    parser::ast::{Function, FunctionParameter},
     typechecker::{Type, ValidatedTypeInformation},
 };
 
@@ -39,11 +39,24 @@ impl<'ctx> CodeGen<'ctx> for Function<ValidatedTypeInformation> {
 
         let llvm_fn_type = get_function_type(&llvm_return_type, &llvm_param_types);
 
-        let llvm_fn = ctx.module.add_function(&id.name, llvm_fn_type, None);
-        let llvm_fn_bb = ctx.context.append_basic_block(llvm_fn, "entry");
-        ctx.builder.position_at_end(llvm_fn_bb);
+        // get function value and store it in the scope (such that it can be referenced later)
+        let llvm_fn_value = ctx.module.add_function(&id.name, llvm_fn_type, None);
+        ctx.store_function(&id.name, llvm_fn_value);
 
+        // enter scope for function parameters and local variables
         ctx.enter_scope();
+        for (i, param) in parameters.iter().enumerate() {
+            let FunctionParameter { name, .. } = param;
+
+            let llvm_param_value = llvm_fn_value
+                .get_nth_param(i as u32)
+                .expect("There should be this parameter");
+
+            ctx.store_variable(&name.name, llvm_param_value);
+        }
+
+        let llvm_fn_bb = ctx.context.append_basic_block(llvm_fn_value, "entry");
+        ctx.builder.position_at_end(llvm_fn_bb);
 
         for statement in statements {
             statement.codegen(ctx);
